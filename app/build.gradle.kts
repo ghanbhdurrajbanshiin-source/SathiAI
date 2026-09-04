c57b268f9034d4c37fed54382ac839d9fd35c6a3
ghanbhdurrajbanshiin-source <ghanbhdurrajbanshi.in@gmail.com>
2026-09-04 21:24:04 +0545

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,22 @@ plugins {
 android {
     namespace = "com.sathiai.app"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.sathiai.app"
         minSdk = 26
         targetSdk = 35
         versionCode = 2
         versionName = "0.2.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
